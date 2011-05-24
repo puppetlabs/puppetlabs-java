55a2ab8b198b806e2e8866fc46165e4a10ebe043
Jeff McCune <jeff@puppetlabs.com>
2011-05-23 18:12:37 -0700

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+java
+
+This is the java module.
---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: java
+#
+# This module manages java
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class java {
+
+
+}
