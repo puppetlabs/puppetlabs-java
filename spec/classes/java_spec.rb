ad93b44405c2185ac570177882e8c1e3b75a2529
Sharif Nassar <sharif@mediatemple.net>
2013-06-18 14:12:36 -0700

    Support multiple JVM, and Debian alternatives.
    Add ability to customize which JVM is installed on Debian.
    Additionally, configure alternatives on Debian.

--- spec/classes/java_spec.rb
+++ spec/classes/java_spec.rb
@@ -21,11 +21,46 @@ describe 'java', :type => :class do
     let(:facts) { {:osfamily => 'RedHat', :operatingsystem => 'Centos', :operatingsystemrelease => '5.3'} }
     let(:params) { { 'package' => 'jdk' } }
     it { should contain_package('java').with_name('jdk') }
+    it { should_not contain_exec('update-java-alternatives') }
   end
-  
+
   context 'select default for Centos 5.3' do
     let(:facts) { {:osfamily => 'RedHat', :operatingsystem => 'Centos', :operatingsystemrelease => '5.3'} }
     it { should contain_package('java').with_name('java-1.6.0-openjdk-devel') }
+    it { should_not contain_exec('update-java-alternatives') }    
+  end
+
+  context 'select default for Debian Wheezy' do
+    let(:facts) { {:osfamily => 'Debian', :operatingsystem => 'Debian', :lsbdistcodename => 'wheezy', :operatingsystemrelease => '7.1', :architecture => 'amd64',} }
+    it { should contain_package('java').with_name('openjdk-7-jdk') }
+    it { should contain_exec('update-java-alternatives').with_command('update-java-alternatives --set java-1.7.0-openjdk-amd64 --jre') }    
+  end
+
+  context 'select Oracle JRE for Debian Wheezy' do
+    let(:facts) { {:osfamily => 'Debian', :operatingsystem => 'Debian', :lsbdistcodename => 'wheezy', :operatingsystemrelease => '7.1', } }
+    let(:params) { { 'distribution' => 'oracle-jre' } }
+    it { should contain_package('java').with_name('oracle-j2re1.7') }
+    it { should contain_exec('update-java-alternatives').with_command('update-java-alternatives --set j2re1.7-oracle --jre') }
+  end
+
+  context 'select default for Debian Squeeze' do
+    let(:facts) { {:osfamily => 'Debian', :operatingsystem => 'Debian', :lsbdistcodename => 'squeeze', :operatingsystemrelease => '6.0.5'} }
+    it { should contain_package('java').with_name('openjdk-6-jdk') }
+    it { should contain_exec('update-java-alternatives').with_command('update-java-alternatives --set java-6-openjdk --jre') }    
+  end
+
+  context 'select Oracle JRE for Debian Squeeze' do
+    let(:facts) { {:osfamily => 'Debian', :operatingsystem => 'Debian', :lsbdistcodename => 'squeeze', :operatingsystemrelease => '6.0.5'} }
+    let(:params) { { 'distribution' => 'sun-jre', } }
+    it { should contain_package('java').with_name('sun-java6-jre') }
+    it { should contain_exec('update-java-alternatives').with_command('update-java-alternatives --set java-6-sun --jre') }
+  end
+
+  context 'select random alternative for Debian Wheezy' do
+    let(:facts) { {:osfamily => 'Debian', :operatingsystem => 'Debian', :lsbdistcodename => 'wheezy', :operatingsystemrelease => '7.1'} }
+    let(:params) { { 'java_alternative' => 'bananafish' } }
+    it { should contain_package('java').with_name('openjdk-7-jdk') }
+    it { should contain_exec('update-java-alternatives').with_command('update-java-alternatives --set bananafish --jre') }
   end
 
 end
