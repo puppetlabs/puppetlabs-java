5b45d1fede2648f7cefdcb31c7fb7986ad8533cc
Colleen Murphy <colleen@puppetlabs.com>
2015-03-18 13:43:43 -0700

    Update rspec tests for rspec-puppet 2
    This patch uses `catalogue` instead of `subject` for the raise_error
    matcher since `subject` is not evaluated the way it used to be. We also
    unpin rspec-puppet.

--- spec/classes/java_spec.rb
+++ spec/classes/java_spec.rb
@@ -181,7 +181,7 @@ describe 'java', :type => :class do
     ].each do |facts|
       let(:facts) { facts }
       it "should fail on #{facts[:operatingsystem]} #{facts[:operatingsystemrelease]}" do
-        expect { subject }.to raise_error Puppet::Error, /unsupported platform/
+        expect { catalogue }.to raise_error Puppet::Error, /unsupported platform/
       end
     end
   end
