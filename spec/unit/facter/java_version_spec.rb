797677caf7e2c6fcbcc1659f43e95cfc5f45cbf9
Vadym Chepkov <vchepkov@gmail.com>
2017-04-19 07:32:20 -0400

    [MODULES-4736] Increase Xmx setting for java_version fact
    Previous value of 8M caused JVM to crash on servers with more then 40G of RAM
    Tested on databases servers up to 1.5T of RAM, 12M seems to be sufficient

--- spec/unit/facter/java_version_spec.rb
+++ spec/unit/facter/java_version_spec.rb
@@ -19,7 +19,7 @@ OpenJDK Runtime Environment (build 1.7.0_71-b14)
 OpenJDK 64-Bit Server VM (build 24.71-b01, mixed mode)
           EOS
           Facter::Util::Resolution.expects(:which).with("java").returns('/usr/local/jdk-1.7.0/jre/bin/java')
-          Facter::Util::Resolution.expects(:exec).with("java -Xmx8m -version 2>&1").returns(java_version_output)
+          Facter::Util::Resolution.expects(:exec).with("java -Xmx12m -version 2>&1").returns(java_version_output)
           expect(Facter.value(:java_version)).to eq("1.7.0_71")
         end
       end
@@ -35,7 +35,7 @@ Java(TM) SE Runtime Environment (build 1.7.0_71-b14)
 Java HotSpot(TM) 64-Bit Server VM (build 24.71-b01, mixed mode)
           EOS
           Facter::Util::Resolution.expects(:exec).with("/usr/libexec/java_home --failfast 2>&1").returns("/Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home")
-          Facter::Util::Resolution.expects(:exec).with("java -Xmx8m -version 2>&1").returns(java_version_output)
+          Facter::Util::Resolution.expects(:exec).with("java -Xmx12m -version 2>&1").returns(java_version_output)
           expect(Facter.value(:java_version)).to eql "1.7.0_71"
         end
       end
@@ -50,7 +50,7 @@ java version "1.7.0_71"
 Java(TM) SE Runtime Environment (build 1.7.0_71-b14)
 Java HotSpot(TM) 64-Bit Server VM (build 24.71-b01, mixed mode)
           EOS
-          Facter::Util::Resolution.expects(:exec).with("java -Xmx8m -version 2>&1").returns(java_version_output)
+          Facter::Util::Resolution.expects(:exec).with("java -Xmx12m -version 2>&1").returns(java_version_output)
           expect(Facter.value(:java_version)).to eq("1.7.0_71")
         end
       end
