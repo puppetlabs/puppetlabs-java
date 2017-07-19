ccba7c0996fd5a24e9495b6c8add1fe4c24bed37
solomon-s-b <salomon.s.b@gmail.com>
2017-07-19 08:54:08 +0300

    Update init.pp
    Removed comments that where put in front of java class variables.

--- manifests/init.pp
+++ manifests/init.pp
@@ -45,13 +45,13 @@
 # Sample Usage:
 #
 class java(
-  String $distribution                                              = 'jdk',
-  Pattern[/present|installed|latest|^[.+_0-9a-zA-Z:~-]+$/] $version = 'present',
-  Optional[String] $package                                         = undef,
-  Optional[Array] $package_options                                  = undef,
-  Optional[String] $java_alternative                                = undef,
-  Optional[String] $java_alternative_path                           = undef,
-  Optional[String] $java_home                                       = undef
+  $distribution           = 'jdk',
+  $version                = 'present',
+  $package                = undef,
+  $package_options        = undef,
+  $java_alternative       = undef,
+  $java_alternative_path  = undef,
+  $java_home              = undef
 ) {
   include java::params
 
