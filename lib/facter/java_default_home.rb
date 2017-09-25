90e247199e2383f1b036bd8fbded82c2b03eae2f
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2017-09-25 12:05:00 +0200

    OpenBSD doesn't have /etc/environment, therefore there is no need to fiddle with it. The default case statement, "do nothing" is all fine here.
    Facter::Util::Resolution.with_env is long time gone since
    Facter > 2.x. That was even longer before introduced by me.
    Remove that OpenBSD special case in the java_version fact,
    and assume that $PATH is properly set in order to find
    the 'java' binary.
    
    Additionally confine the java_default_home and java_libjvm_path
    to OpenBSD kernel as well. Add some flesh to the java_default_home
    fact to allow it to find the java binary.

--- lib/facter/java_default_home.rb
+++ lib/facter/java_default_home.rb
@@ -11,13 +11,20 @@
 # Notes:
 #   None
 Facter.add(:java_default_home) do
-  confine :kernel => 'Linux'
+  confine :kernel => [ 'Linux', 'OpenBSD' ]
   setcode do
     if Facter::Util::Resolution.which('readlink')
-      java_bin = Facter::Util::Resolution.exec('readlink -e /usr/bin/java').strip
+      java_bin = Facter::Util::Resolution.exec('readlink -e /usr/bin/java').to_s.strip
       if java_bin.empty?
-        nil
-      elsif java_bin =~ %r(/jre/)
+        java_bin = Facter::Util::Resolution.exec('readlink -e /usr/local/bin/java').to_s.strip
+        if java_bin.empty?
+          java_bin = Facter::Util::Resolution.which('java').to_s.strip
+          if java_bin.empty?
+            nil
+          end
+        end
+      end
+      if java_bin =~ %r(/jre/)
         java_default_home = File.dirname(File.dirname(File.dirname(java_bin)))
       else
         java_default_home = File.dirname(File.dirname(java_bin))
