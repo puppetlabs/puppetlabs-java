24ca4a662a2b28ab5c140228ba53220b762588c4
timdeluxe <tim.eilers@web.de>
2020-07-14 17:28:48 +0200

    Support SAP Javas (sapjvm / sapmachine)

---
+++ manifests/sap.pp
@@ -0,0 +1,227 @@
+# Defined Type java::sap
+#
+# @summary
+#   Install one or more versions of SAPJVM or Sapmachine
+#
+# @param ensure
+#   Install or remove the package.
+#
+# @param version
+#   Version of Java to install, e.g. '8' or '9'. Default values for full versions will be used.
+#
+# @param version_full
+#   Major version which should be installed, e.g. '8.1.063' or '11.0.7'. If used, "version" parameter is ignored.
+#
+# @param java
+#   Type of Java Edition to install, jdk or jre.
+#
+# @param proxy_server
+#   Specify a proxy server, with port number if needed. ie: https://example.com:8080. (passed to archive)
+#
+# @param proxy_type
+#   Proxy server type (none|http|https|ftp). (passed to archive)
+#
+# @param basedir
+#   Directory under which the installation will occur. If not set, defaults to
+#   /usr/lib/jvm for Debian and /usr/java for RedHat.
+#
+# @param manage_basedir
+#   Whether to manage the basedir directory.  Defaults to false.
+#   Note: /usr/lib/jvm is managed for Debian by default, separate from this parameter.
+#
+# @param manage_symlink
+#   Whether to manage a symlink that points to the installation directory.  Defaults to false.
+#
+# @param symlink_name
+#   The name for the optional symlink in the installation directory.
+#
+define java::sap (
+  $ensure         = 'present',
+  $version        = '8',
+  $version_full   = undef,
+  $java           = 'jdk',
+  $proxy_server   = undef,
+  $proxy_type     = undef,
+  $basedir        = undef,
+  $manage_basedir = true,
+  $manage_symlink = false,
+  $symlink_name   = undef,
+) {
+
+  # archive module is used to download the java package
+  include ::archive
+
+  # validate java edition to download
+  if $java !~ /(jre|jdk)/ {
+    fail('java must be either jre or jdk.')
+  }
+
+  # determine version and installation path
+  if $version_full {
+
+    $_version_array = $version_full.scanf('%i')
+    $_version_int = $_version_array[0]
+
+    $_version_full = $version_full
+
+  } else {
+    $_version = $version
+    $_version_int = Numeric($_version)
+    # use default versions if full version parameter is not provided
+    case $version {
+      '7' : {
+        $_version_full = '7.1.070'
+        if ($java != 'jdk') {
+          fail('java parameter is not jdk. jre is not supported on version 7')
+        }
+      }
+      '8' : {
+        $_version_full = '8.1.063'
+        if ($java != 'jdk') {
+          fail('java parameter is not jdk. jre is not supported on version 8')
+        }
+      }
+      '11' : {
+        $_version_full = '11.0.7'
+      }
+      '14' : {
+        $_version_full = '14.0.1'
+      }
+      default : {
+        fail("${version} not yet supported by module")
+      }
+    }
+  }
+
+  # extracted folders look like this:
+  #  sapjvm_8
+  #  sapmachine-jdk-11.0.7
+  if ($_version_int == 7 or $_version_int == 8) {
+    $_creates_folder = "sapjvm_${_version_int}"
+  } else {
+    $_creates_folder = "sapmachine-${java}-${_version_full}"
+  }
+
+  # determine destination directory based on OS
+  case $facts['kernel'] {
+    'Linux' : {
+      case $facts['os']['family'] {
+        'RedHat', 'Amazon' : {
+          if $basedir {
+            $_basedir = $basedir
+          } else {
+            $_basedir = '/usr/java'
+          }
+        }
+        'Debian' : {
+          if $basedir {
+            $_basedir = $basedir
+          } else {
+            $_basedir = '/usr/lib/jvm'
+          }
+        }
+        default : {
+          fail ("unsupported os family ${$facts['os']['name']}") }
+      }
+
+      $creates_path = "${_basedir}/${_creates_folder}"
+    }
+    default : {
+      fail ( "unsupported platform ${$facts['kernel']}" ) }
+  }
+
+  $_os_architecture = $facts['os']['architecture'] ? {
+    undef => $facts['architecture'],
+    default => $facts['os']['architecture']
+  }
+
+  if ($_os_architecture != 'x86_64' and $_os_architecture != 'amd64') {
+    fail ("unsupported platform ${_os_architecture}")
+  }
+
+  # download links look like this (examples):
+  # https://tools.hana.ondemand.com/additional/sapjvm-8.1.063-linux-x64.zip
+  # https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.7/sapmachine-jre-11.0.7_linux-x64_bin.tar.gz
+  # https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.7/sapmachine-jdk-11.0.7_linux-x64_bin.tar.gz
+  # https://github.com/SAP/SapMachine/releases/download/sapmachine-14.0.1/sapmachine-jdk-14.0.1_linux-x64_bin.tar.gz
+
+  # cookie is currently at version 3.1, but may be changed one day. It is only required for download at SAP.
+  # by using this module you agree with the EULA presented at tools.hana.ondemand.com download page!
+  # Github does not require it
+
+  if ( $_version_int == 7 or $_version_int == 8 ) {
+    # sapjvm download
+    $archive_filename = "sapjvm-${_version_full}-linux-x64.zip"
+    $source = "https://tools.hana.ondemand.com/additional/${archive_filename}"
+    $cookie = 'eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt'
+
+    if (!defined(Package['unzip'])) {
+      package { 'unzip':
+        ensure => 'present',
+        before => Archive["/tmp/${archive_filename}"],
+      }
+    }
+  } else {
+    $archive_filename = "sapmachine-${java}-${_version_full}_linux-x64_bin.tar.gz"
+    $source = "https://github.com/SAP/SapMachine/releases/download/sapmachine-${_version_full}/${archive_filename}"
+    $cookie = undef
+
+    if (!defined(Package['tar'])) {
+      package { 'tar':
+        ensure => 'present',
+        before => Archive["/tmp/${archive_filename}"],
+      }
+    }
+    if (!defined(Package['gzip'])) {
+      package { 'gzip':
+        ensure => 'present',
+        before => Archive["/tmp/${archive_filename}"],
+      }
+    }
+  }
+
+  case $ensure {
+    'present' : {
+      case $facts['kernel'] {
+        'Linux' : {
+          if ($manage_basedir or $facts['os']['family'] == 'Debian'){
+            if (!defined(File[$_basedir])) {
+              file { $_basedir:
+                ensure => 'directory',
+                before => Archive["/tmp/${archive_filename}"],
+              }
+            }
+          }
+
+          archive { "/tmp/${archive_filename}" :
+            ensure       => present,
+            source       => $source,
+            extract      => true,
+            extract_path => $_basedir,
+            cleanup      => false,
+            creates      => $creates_path,
+            cookie       => $cookie,
+            proxy_server => $proxy_server,
+            proxy_type   => $proxy_type,
+          }
+
+          if ($manage_symlink and $symlink_name) {
+            file { "${_basedir}/${symlink_name}":
+              ensure  => link,
+              target  => $creates_path,
+              require => Archive["/tmp/${archive_filename}"],
+            }
+          }
+
+        }
+        default : {
+          fail ("unsupported platform ${$facts['kernel']}")
+        }
+      }
+    }
+    default : {
+      notice ("Action ${ensure} not supported.")
+    }
+  }
+
+}
---
+++ spec/defines/sap_spec.rb
@@ -0,0 +1,255 @@
+require 'spec_helper'
+
+describe 'java::sap', type: :define do
+  context 'with CentOS 64-bit' do
+    let(:facts) { { kernel: 'Linux', os: { family: 'RedHat', architecture: 'x86_64', name: 'CentOS', release: { full: '6.0' } } } }
+
+    context 'when manage_symlink is set to true' do
+      let(:params) do
+        {
+          ensure: 'present',
+          version: '11',
+          java: 'jdk',
+          basedir: '/usr/java',
+          manage_symlink: true,
+          symlink_name: 'java_home',
+        }
+      end
+      let(:title) { 'jdk11_symlink' }
+
+      it { is_expected.to contain_file('/usr/java/java_home') }
+    end
+
+    context 'when manage_symlink is not set' do
+      let(:params) { { ensure: 'present', version: '11', java: 'jdk' } }
+      let(:title) { 'jdk11_nosymlink' }
+
+      it { is_expected.not_to contain_file('/usr/java/java_home') }
+    end
+
+    context 'when sapjvm 7' do
+      let(:params) { { ensure: 'present', version: '7', java: 'jdk' } }
+      let(:title) { 'jdk7' }
+
+      it { is_expected.to contain_archive('/tmp/sapjvm-7.1.070-linux-x64.zip') }
+    end
+
+    context 'when sapjvm 8' do
+      let(:params) { { ensure: 'present', version: '8', java: 'jdk' } }
+      let(:title) { 'jdk8' }
+
+      it { is_expected.to contain_archive('/tmp/sapjvm-8.1.063-linux-x64.zip') }
+    end
+
+    context 'when sapmachine 11 jdk' do
+      let(:params) { { ensure: 'present', version: '11', java: 'jdk' } }
+      let(:title) { 'jdk11' }
+
+      it { is_expected.to contain_archive('/tmp/sapmachine-jdk-11.0.7_linux-x64_bin.tar.gz') }
+    end
+
+    context 'when sapmachine 11 jre' do
+      let(:params) { { ensure: 'present', version: '11', java: 'jre' } }
+      let(:title) { 'jre11' }
+
+      it { is_expected.to contain_archive('/tmp/sapmachine-jre-11.0.7_linux-x64_bin.tar.gz') }
+    end
+
+    context 'when sapmachine 14 jdk' do
+      let(:params) { { ensure: 'present', version: '14', java: 'jdk' } }
+      let(:title) { 'jdk14' }
+
+      it { is_expected.to contain_archive('/tmp/sapmachine-jdk-14.0.1_linux-x64_bin.tar.gz') }
+    end
+
+    context 'when sapmachine 14 jre' do
+      let(:params) { { ensure: 'present', version: '14', java: 'jre' } }
+      let(:title) { 'jre14' }
+
+      it { is_expected.to contain_archive('/tmp/sapmachine-jre-14.0.1_linux-x64_bin.tar.gz') }
+    end
+
+    context 'when installing multiple versions' do
+      let(:params) do
+        {
+          ensure: 'present',
+          version_full: '11.0.7',
+          java: 'jdk',
+        }
+      end
+      let(:title) { 'jdk1107' }
+
+      let(:pre_condition) do
+        <<-EOL
+        java::sap {
+          'jdk1106':
+            ensure       => 'present',
+            version_full => '11.0.6',
+            java         => 'jdk',
+        }
+        EOL
+      end
+
+      it { is_expected.to compile }
+    end
+
+    context 'when specifying basedir' do
+      let(:params) do
+        {
+          ensure: 'present',
+          version: '8',
+          java: 'jdk',
+          basedir: '/usr/java',
+        }
+      end
+      let(:title) { 'jdk8' }
+
+      it { is_expected.to contain_archive('/tmp/sapjvm-8.1.063-linux-x64.zip') }
+    end
+    context 'when manage_basedir is set to true' do
+      let(:params) do
+        {
+          ensure: 'present',
+          version: '8',
+          java: 'jdk',
+          basedir: '/usr/java',
+          manage_basedir: true,
+        }
+      end
+      let(:title) { 'jdk8' }
+
+      it { is_expected.to contain_file('/usr/java') }
+    end
+  end
+
+  context 'with Ubuntu 64-bit' do
+    let(:facts) { { kernel: 'Linux', os: { family: 'Debian', architecture: 'amd64', name: 'Ubuntu', release: { full: '16.04' } } } }
+
+    context 'when sapjvm 7' do
+      let(:params) { { ensure: 'present', version: '7', java: 'jdk' } }
+      let(:title) { 'jdk7' }
+
+      it { is_expected.to contain_archive('/tmp/sapjvm-7.1.070-linux-x64.zip') }
+    end
+
+    context 'when sapjvm 8' do
+      let(:params) { { ensure: 'present', version: '8', java: 'jdk' } }
+      let(:title) { 'jdk8' }
+
+      it { is_expected.to contain_archive('/tmp/sapjvm-8.1.063-linux-x64.zip') }
+    end
+
+    context 'when sapmachine 11 jdk' do
+      let(:params) { { ensure: 'present', version: '11', java: 'jdk' } }
+      let(:title) { 'jdk11' }
+
+      it { is_expected.to contain_archive('/tmp/sapmachine-jdk-11.0.7_linux-x64_bin.tar.gz') }
+    end
+
+    context 'when sapmachine 11 jre' do
+      let(:params) { { ensure: 'present', version: '11', java: 'jre' } }
+      let(:title) { 'jre11' }
+
+      it { is_expected.to contain_archive('/tmp/sapmachine-jre-11.0.7_linux-x64_bin.tar.gz') }
+    end
+
+    context 'when sapmachine 14 jdk' do
+      let(:params) { { ensure: 'present', version: '14', java: 'jdk' } }
+      let(:title) { 'jdk14' }
+
+      it { is_expected.to contain_archive('/tmp/sapmachine-jdk-14.0.1_linux-x64_bin.tar.gz') }
+    end
+
+    context 'when sapmachine 14 jre' do
+      let(:params) { { ensure: 'present', version: '14', java: 'jre' } }
+      let(:title) { 'jre14' }
+
+      it { is_expected.to contain_archive('/tmp/sapmachine-jre-14.0.1_linux-x64_bin.tar.gz') }
+    end
+
+    context 'when installing multiple versions' do
+      let(:params) do
+        {
+          ensure: 'present',
+          version_full: '11.0.7',
+          java: 'jdk',
+        }
+      end
+      let(:title) { 'jdk1107' }
+
+      let(:pre_condition) do
+        <<-EOL
+        java::sap {
+          'jdk1106':
+            ensure       => 'present',
+            version_full => '11.0.6',
+            java         => 'jdk',
+        }
+        EOL
+      end
+
+      it { is_expected.to compile }
+    end
+  end
+  describe 'incompatible OSes' do
+    [
+      {
+        kernel: 'Windows',
+        os: {
+          family: 'Windows',
+          name: 'Windows',
+          release: {
+            full: '8.1',
+          },
+        },
+      },
+      {
+        kernel: 'Darwin',
+        os: {
+          family: 'Darwin',
+          name: 'Darwin',
+          release: {
+            full: '13.3.0',
+          },
+        },
+      },
+      {
+        kernel: 'AIX',
+        os: {
+          family: 'AIX',
+          name: 'AIX',
+          release: {
+            full: '7100-02-00-000',
+          },
+        },
+      },
+      {
+        kernel: 'AIX',
+        os: {
+          family: 'AIX',
+          name: 'AIX',
+          release: {
+            full: '6100-07-04-1216',
+          },
+        },
+      },
+      {
+        kernel: 'AIX',
+        os: {
+          family: 'AIX',
+          name: 'AIX',
+          release: {
+            full: '5300-12-01-1016',
+          },
+        },
+      },
+    ].each do |facts|
+      let(:facts) { facts }
+      let(:title) { 'jdk' }
+
+      it "is_expected.to fail on #{facts[:os][:name]} #{facts[:os][:release][:full]}" do
+        expect { catalogue }.to raise_error Puppet::Error, %r{unsupported platform}
+      end
+    end
+  end
+end
